e272a223054c413a711ef59d2976fe1f1dc8851f
siddiqui.rahil <siddiqui.rahil@LT-276-HO>
2024-01-23 15:04:03 +0530

    Pushed

--- POS.Data/Entities/Cart/Cart.cs
+++ POS.Data/Entities/Cart/Cart.cs
@@ -23,9 +23,7 @@ namespace POS.Data
         public decimal DiscountPercentage { get; set; }
         public Product Product { get; set; }
         public bool? IsAdvanceOrderRequest { get; set; }
-        public Guid? ProductMainCategoryId { get; set; }
-        public string PackagingName { get; set; }
-        public Guid? PackagingId { get; set; }
+        public Guid? ProductMainCategoryId { get; set; }        
 
     }
 }
