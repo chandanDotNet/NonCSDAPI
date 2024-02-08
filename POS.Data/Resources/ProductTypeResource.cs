6a928c820603143059cf44760142eedeb8f50426
siddiqui.rahil <siddiqui.rahil@LT-276-HO>
2024-02-08 14:55:18 +0530

    Rahil_08-02-2024

---
+++ POS.Data/Resources/ProductTypeResource.cs
@@ -0,0 +1,20 @@
+﻿using POS.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Data.Resources
+{
+    public class ProductTypeResource : ResourceParameters
+    {
+        public ProductTypeResource() : base("ProductTypeName")
+        {
+
+        }
+        public Guid? Id { get; set; }
+        //public string ManufacturerName { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
