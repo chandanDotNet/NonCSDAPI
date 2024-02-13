b01d37deed90d782237963e0832d56a3ab22f46b
siddiqui.rahil <siddiqui.rahil@LT-276-HO>
2024-02-13 13:26:21 +0530

    Rahil_13-02-2024

---
+++ POS.Data/Dto/Year/YearDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Data.Dto
+{
+    public class YearDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool DefaultYear { get; set; }
+    }
+}
---
+++ POS.MediatR/Year/Handler/GetAllYearQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using POS.Data.Dto;
+using POS.MediatR.CommandAndQuery;
+using POS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POS.MediatR.Year.Handler
+{
+    public class GetAllYearQueryHandler : IRequestHandler<GetAllYearQuery, List<YearDto>>
+    {
+        private readonly IYearRepository _yearRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllYearQueryHandler(IYearRepository yearRepository, IMapper mapper
+            )
+        {
+            _yearRepository = yearRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<YearDto>> Handle(GetAllYearQuery request, CancellationToken cancellationToken)
+        {
+            var years = await _yearRepository.All
+               .Select(c => new YearDto
+               {
+                   Id = c.Id,
+                   Name = c.Name,
+                   DefaultYear= c.DefaultYear
+               })               
+               .OrderBy(c => c.Name)
+               .ToListAsync();
+
+            return years;
+        }
+    }
+}
